b9e353efa7ee51492f52879bd6efbe0e5de155b1
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-03-23 08:45:20 -0300

    mode SUT creation to a factory method

---
+++ documentation/notes.md
@@ -0,0 +1,37 @@
+# Pattern
+
+## Anti-Patterns/Code Smells
+
+- Speculative Generality
+  - Utilizar ferramentar especuladas que será utilizada no futuro
+- God Class
+  - Classes que são muito grandes e complexas ou que façam de tudo
+- Divergent Change
+  - Componente que precisa ser mexido por mais de um motivo
+- Improper Instantiations
+  - Instâncias de classes de forma errada
+- High Coupling
+  - Quando uma classe cria sua própria independência
+- Test Code in Production
+  - Testes que são executados em produção
+- Duplicate code
+  - Código duplicado
+- Shotgun Surgery
+  - Quando mexemos em um lugar e afeta vários outros lugares
+
+## Design Patterns/Principles/Conventions
+
+- You Ain't Gonna Need It (YAGNI)
+- Single Responsibility (SRP)
+- Liskov Substitution (LSP)
+- Dependency Inversion (DIP)
+- Arrange, Act, Assert (AAA)
+- Dependency Injection (DI)
+- Repository Pattern
+- Test Doubles (Mock, Stub, Spy)
+  - Mock -> Preocupado com input
+  - Stub -> Preocupado com output
+  - Spy -> Preocupado com input e output
+- Small Commits
+- Sustem Under Test (SUT)
+- Strategy Pattern
---
+++ tests/check-last-event-status.spec.ts
@@ -0,0 +1,57 @@
+class CheckLastEventStatus {
+  constructor(
+    private readonly loadLastEventRepository: LoadLastEventRepository,
+  ) {}
+
+  async perform(groupId: string): Promise<string> {
+    await this.loadLastEventRepository.loadLastEvent(groupId);
+    return 'done';
+  }
+}
+
+interface LoadLastEventRepository {
+  loadLastEvent: (groupId: string) => Promise<undefined>;
+}
+
+class LoadLastEventRepositorySpy implements LoadLastEventRepository {
+  groupId?: string;
+  callsCount = 0;
+  output: undefined;
+
+  async loadLastEvent(groupId: string): Promise<undefined> {
+    this.groupId = groupId;
+    this.callsCount++;
+    return this.output;
+  }
+}
+
+type SutOutput = {
+  sut: CheckLastEventStatus;
+  loadLastEventRepository: LoadLastEventRepositorySpy;
+};
+
+const makeSut = (): SutOutput => {
+  const loadLastEventRepository = new LoadLastEventRepositorySpy();
+  const sut = new CheckLastEventStatus(loadLastEventRepository);
+  return { sut, loadLastEventRepository };
+};
+
+describe('CheckLastEventStatus', () => {
+  it('should get last event data', async () => {
+    const { sut, loadLastEventRepository } = makeSut();
+
+    await sut.perform('any_group_id');
+
+    expect(loadLastEventRepository.groupId).toBe('any_group_id');
+    expect(loadLastEventRepository.callsCount).toBe(1);
+  });
+
+  it('should return status done when group has no event', async () => {
+    const { sut, loadLastEventRepository } = makeSut();
+    loadLastEventRepository.output = undefined;
+
+    const status = await sut.perform('any_group_id');
+
+    expect(status).toBe('done');
+  });
+});
